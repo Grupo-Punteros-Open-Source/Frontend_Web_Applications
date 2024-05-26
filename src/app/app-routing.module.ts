77b6b37fe3b6b306ec2985f03e9c0a839a6e0ea6
Christian <150555815+ChristoTF2@users.noreply.github.com>
2024-05-26 16:44:11 -0500

    feat: added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import {InvoiceListComponent} from "./manager/invoices/pages/invoice-list/invoic
 import {AddInvoiceComponent} from "./manager/invoices/components/add-invoice/add-invoice.component";
 import {InvoicePreviewComponent} from "./manager/invoices/components/invoice-preview/invoice-preview.component";
 import {HomeManagerComponent} from "./public/pages/home-manager/home-manager.component";
+import {AddProductsComponent} from "./manager/products/components/add-products/add-products.component";
+import {UpdateProductComponent} from "./manager/products/components/update-product/update-product.component";
+import {ProductCardComponent} from "./manager/products/components/product-card/product-card.component";
+import {ProductDetailsComponent} from "./manager/products/components/product-details/product-details.component";
 
 
 const routes: Routes = [
@@ -11,7 +15,11 @@ const routes: Routes = [
   {path:'workshop', component: HomeManagerComponent},
   {path:'workshop/invoices', component: InvoiceListComponent},
   {path:'workshop/invoices/add-invoice', component: AddInvoiceComponent},
-  {path:'workshop/invoices/invoice-preview/:clientId/:invoiceId', component: InvoicePreviewComponent}
+  {path:'workshop/invoices/invoice-preview/:clientId/:invoiceId', component: InvoicePreviewComponent},
+  {path:'workshop/products/add-products', component: AddProductsComponent},
+  {path:'workshop/products/update-product', component: UpdateProductComponent},
+  {path:'workshop/products/product-card', component: ProductCardComponent},
+  { path: 'workshop/products/product-detail/:id', component: ProductDetailsComponent }
 ];
 
 @NgModule({
