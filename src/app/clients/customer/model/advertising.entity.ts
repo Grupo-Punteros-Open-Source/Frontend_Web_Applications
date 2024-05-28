3c4d84f780669062a524e9cc97748be94a0c3434
Christian <150555815+ChristoTF2@users.noreply.github.com>
2024-05-28 09:47:36 -0500

    fix: db advertising

--- src/app/clients/customer/model/advertising.entity.ts
+++ src/app/clients/customer/model/advertising.entity.ts
@@ -6,13 +6,15 @@ export class Advertising {
   slogan: string;
   Msg1: string;
   Msg2: string;
+  Msg3: string;
 
-  constructor(id: string, workshopId: string, image_url: string, slogan: string, Msg1: string, Msg2: string) {
+  constructor(id: string, workshopId: string, image_url: string, slogan: string, Msg1: string, Msg2: string, Msg3: string) {
     this.id = id;
     this.workshopId = workshopId;
     this.image_url = image_url;
     this.slogan = slogan;
     this.Msg1 = Msg1;
     this.Msg2 = Msg2;
+    this.Msg3 = Msg3
   }
 }
