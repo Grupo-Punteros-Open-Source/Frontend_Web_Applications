0ca057b680b34144b59e61edf58e9a826be52548
Gareth <sebastianmv2019@gmail.com>
2024-05-30 04:30:42 -0500

    feat: added routes for vehicles components and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import {InvoiceListComponent} from "./manager/invoices/pages/invoice-list/invoic
 import {AddInvoiceComponent} from "./manager/invoices/components/add-invoice/add-invoice.component";
 import {InvoicePreviewComponent} from "./manager/invoices/components/invoice-preview/invoice-preview.component";
 import {HomeManagerComponent} from "./public/pages/home-manager/home-manager.component";
+import {ClientVehicleListComponent} from "./manager/vehicles/pages/client-vehicle-list/client-vehicle-list.component";
+import {AddVehicleComponent} from "./manager/vehicles/pages/add-vehicle/add-vehicle.component";
 
 
 const routes: Routes = [
@@ -11,7 +13,9 @@ const routes: Routes = [
   {path:'workshop', component: HomeManagerComponent},
   {path:'workshop/invoices', component: InvoiceListComponent},
   {path:'workshop/invoices/add-invoice', component: AddInvoiceComponent},
-  {path:'workshop/invoices/invoice-preview/:clientId/:invoiceId', component: InvoicePreviewComponent}
+  {path:'workshop/invoices/invoice-preview/:clientId/:invoiceId', component: InvoicePreviewComponent},
+  {path:'workshop/customer/:clientId/vehicle-list', component: ClientVehicleListComponent},
+  {path:'workshop/customer/:clientId/add-vehicle', component: AddVehicleComponent}
 ];
 
 @NgModule({
