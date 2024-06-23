4f7b9c9e12035a1620364ff356644b8f645325ea
Alvaro <129535538+AlvaroPFR@users.noreply.github.com>
2024-06-22 20:12:27 -0500

    feat(customerVehicle): added current user id detection while showing the customer's vehicles

--- src/app/Customer/components/vehicle/vehicle.component.ts
+++ src/app/Customer/components/vehicle/vehicle.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomerVehicleService } from "../../services/customer-vehicle.service";
-import { CustomerVehicleEntity } from "../../model/customer-vehicle.entity";
-import {Observer} from "rxjs";
-import {User} from "../../../User/model/user.entity";
-import {Customer} from "../../../User/model/customer.entity";
-import {Maintenance} from "../../../Maintenance/model/maintenance";
+import { VehicleService } from '../../../Maintenance/services/vehicle.service';
+import { AuthenticationService } from '../../../User/services/authentication.service';
+import {CustomerVehicleEntity} from "../../model/customer-vehicle.entity";
 import {Vehicle} from "../../../Maintenance/model/vehicle.entitty";
+import {Maintenance} from "../../../Maintenance/model/maintenance";
+import {Router} from "@angular/router";
+import {User} from "../../../User/model/user.entity";
+import {Workshop} from "../../../User/model/workshop.entity";
+import {Customer} from "../../../User/model/customer.entity";
 import {CustomerService} from "../../../User/services/customer.service";
-import {MaintenanceService} from "../../../Maintenance/services/maintenance.service";
-import {VehicleService} from "../../../Maintenance/services/vehicle.service";
-import {UserService} from "../../../User/services/user.service";
-import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-vehicle',
@@ -18,23 +16,39 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./vehicle.component.css']
 })
 export class VehicleComponent implements OnInit {
-  user: User = {} as User;
-  customer: Customer = {} as Customer;
-  maintenances: Maintenance[] = [];
   vehicles: Vehicle[] = [];
+  userId: number | null = null; // Variable para almacenar el ID del usuario
+  maintenances: Maintenance[] = [];
+  customer: Customer = {} as Customer;
 
-  constructor(private vehicleService: VehicleService, private router: Router) {
-  }
+  constructor(
+      private vehicleService: VehicleService,
+      private authService: AuthenticationService,
+      private router: Router,
+      private customerService: CustomerService
+
+  ) {}
 
   ngOnInit() {
-    this.getVehicles();
+    this.getUserActive();
   }
 
-  getVehicles(): void {
-    this.vehicleService.getAll().subscribe((vehicles: any) => {
-      this.vehicles = vehicles;
-    });
+  async getUserActive(){
+    let userId = localStorage.getItem('userId');
+    if (userId) {
+      let userid = JSON.parse(userId);
+      this.customerService.getAll().subscribe((response: any) => {
+        this.customer = response.find((customer: Customer) => Number(customer.user_id) === Number(userid));
+        this.vehicleService.getAll().subscribe((response: any) => {
+          this.vehicles = response.filter((vehicle : Vehicle) => Number(vehicle.customer_id) === Number(this.customer.id));
+          console.log(this.vehicles);
+        });
+      });
+
+    }
   }
+
+
   viewMaintenance(id:number): void {
     this.router.navigate(['customer/maintenance', id]);
   }
